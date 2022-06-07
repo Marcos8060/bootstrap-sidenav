72ff41812d18098dddada6a6bf0a5dbe25f13d44
Marcos8060 <marcosgav80@gmail.com>
2022-06-07 13:25:27 +0300

    created a standard sidebar navigation

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit a vitae sapiente voluptatum id, obcaecati fugiat natus ea eveniet? Perferendis ut laborum minima ratione vitae maiores optio deleniti sint quia.</div>
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import MiniDrawer from './Navabar';
-
+import "./App.css";
+import Sidebar from './Sidebar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs  from './About'
+import {
+Services,
+ServicesOne,
+ServicesTwo,
+ServicesThree,
+} from './Services'
+import { Events } from './Events'
+import Contact from './ContactUs'
+import Support from './Support'
 function App() {
-  return (
-    <div className="App">
-      <MiniDrawer />
-    </div>
-  );
+return (
+	<Router>
+	<Sidebar />
+	<Routes>
+		<Route path="/about-us" exact component={AboutUs} />
+		<Route path="/services" exact component={Services} />
+		<Route path="/services/services1" exact component={ServicesOne} />
+		<Route path="/services/services2" exact component={ServicesTwo} />
+		<Route path="/services/services3" exact component={ServicesThree} />
+		<Route path="/contact" exact component={Contact} />
+		<Route path="/events" exact component={Events} />
+		<Route path="/support" exact component={Support} />
+	</Routes>
+	</Router>
+);
 }
 
 export default App;
+
